758948202779d48ce9beed8289ae8853ac680b5c
keperkjr <keperkjr@yahoo.com>
2021-09-03 20:01:28 -0700

    * Added search service index name to app settings * Renamed the site header to Azure Search App

--- FirstSearchApp/Controllers/HomeController.cs
+++ FirstSearchApp/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using FirstAzureSearchApp.Models;
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using FirstAzureSearchApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -21,6 +24,71 @@ namespace FirstAzureSearchApp.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchData model)
+        {
+            try
+            {
+                // Ensure the search string is valid.
+                if (model.searchText == null)
+                {
+                    model.searchText = "";
+                }
+
+                // Make the Azure Cognitive Search call.
+                await RunQueryAsync(model);
+            }
+
+            catch(Exception ex)
+            {
+                Debug.Print(ex.ToString());
+                return View("Error", new ErrorViewModel { RequestId = "1" });
+            }
+            return View(model);
+        }
+
+        private static SearchClient _searchClient;
+        private static SearchIndexClient _indexClient;
+        private static IConfigurationBuilder _builder;
+        private static IConfigurationRoot _configuration;
+
+        private void InitSearch()
+        {
+            // Create a configuration using appsettings.json
+            _builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            _configuration = _builder.Build();
+
+            // Read the values from appsettings.json
+            string searchServiceUri = _configuration["SearchServiceUri"];
+            string queryApiKey = _configuration["SearchServiceQueryApiKey"];
+            string searchIndexName = _configuration["SearchServiceIndexName"];
+
+            // Create a service and index client.
+            _indexClient = new SearchIndexClient(new Uri(searchServiceUri), new AzureKeyCredential(queryApiKey));
+            _searchClient = _indexClient.GetSearchClient(searchIndexName);
+        }
+
+        private async Task<ActionResult> RunQueryAsync(SearchData model)
+        {
+            InitSearch();
+
+            var options = new SearchOptions()
+            {
+                IncludeTotalCount = true
+            };
+
+            // Enter Hotel property names into this list so only these values will be returned.
+            // If Select is empty, all values will be returned, which can be inefficient.
+            options.Select.Add("HotelName");
+            options.Select.Add("Description");
+
+            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+            model.resultList = await _searchClient.SearchAsync<Hotel>(model.searchText, options).ConfigureAwait(false);
+
+            // Display the results.
+            return View("Index", model);
         }
 
         public IActionResult Privacy()
